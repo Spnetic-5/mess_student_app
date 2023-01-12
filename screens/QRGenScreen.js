318a9aa8c90ae349ca0e41b7aa8008f43945ee31
Spnetic-5 <sspowar_b19@el.vjti.ac.in>
2023-01-12 14:04:21 +0530

    QR Gen screen

--- screens/QRGenScreen.js
+++ screens/QRGenScreen.js
@@ -1,106 +1,109 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, ActivityIndicator, Modal } from 'react-native';
-import { BarCodeScanner } from 'expo-barcode-scanner';
+import { Text, View, StyleSheet, TouchableOpacity, Modal } from 'react-native';
+import styled from 'styled-components/native';
+import QRCode from 'react-native-qrcode-svg'
 import { useNavigation } from '@react-navigation/native';
+import { Feather, Ionicons } from "@expo/vector-icons";
+import { Avatar } from "react-native-paper";
 
 export default function QRScanner() {
   const navigation = useNavigation();
-  const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
-  const [success, setSuccess] = useState(null);
-  const [error, setError] = useState(null);
   const [qrData, setqrData] = useState(null);
 
-  useEffect(() => {
-    (async () => {
-      const { status } = await BarCodeScanner.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
-    })();
-  }, []);
+  const MainContainer = styled.View`
+    background-color: white;
+    height: 100%;
+    width: 100%;
+  `;
 
-  const handleBarCodeScanned = ({ type, data }) => {
-    setScanned(true);
-    console.log(data);
-    data?.split(' ')[2]?.length == 9 ? setSuccess(true) : setError(true);
-    setqrData(data);
-    setTimeout(() => {setSuccess(false)}, 3000);
-    setTimeout(() => {setError(false);}, 3000);
-    console.log(error);
-    // alert(`Bar code with type ${type} and data ${data} has been scanned!`);
-  };
-
-  if (hasPermission === null) {
-    return <Text>Requesting for camera permission</Text>;
-  }
-  if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
-  }
+  const UpperContainer = styled.View`
+    height: 30%;
+    width: 100%;
+  `;
 
   return (
-    <View style={styles.container}>
-      <Text style={{alignSelf: 'center', color: '#311E15', fontSize: 18}} color='#F3DACC'> Please Wait! </Text>
+    <View style={{backgroundColor: 'white'}}>
+      <UpperContainer style={styles.upper}>
+      <View
+          style={{
+            flexDirection: "row",
+            marginTop: "40%",
+            maxHeight: 25,
+            marginLeft: "20%",
+            marginRight: "20%",
+            marginBottom: "5%",
+          }}
+        >
+          <TouchableOpacity
+            activeOpacity={0.5}
+            onPress={() => navigation.navigate("Home")}
+          >
+            <Ionicons name="chevron-back" size={25} color="#F3DACC" />
+          </TouchableOpacity>
+          <Text
+            style={{
+              color: "#F3DACC",
+              fontWeight: "bold",
+              fontSize: 20,
+              marginLeft: "30%",
+            }}
+          >
+            Profile
+          </Text>
+        </View>
+        <View
+          style={{
+            zIndex: 5,
+            alignSelf: "center",
+            height: 125,
+            width: 125,
+            borderWidth: 2,
+            borderRadius: 100,
+            marginBottom: "4%",
+            borderColor: "#311E15",
+          }}
+        >
+          <Avatar.Image size={120} source={require("../assets/person.jpg")} />
+        </View>
+      </UpperContainer>
+      <MainContainer style={{marginTop: '5%'}}>
+      {/* <Text style={{alignSelf: 'center', color: '#311E15', fontSize: 18}} color='#F3DACC'> Please Wait! </Text>
       <Text style={{alignSelf: 'center', color: '#311E15', fontSize: 20, fontWeight: '500', marginBottom: '4%'}} color='#F3DACC'> Scanning the QR Code . . . </Text>
-      <ActivityIndicator size="large" color="#8B6C5B" style={{marginBottom: '8%'}}/>
-      <BarCodeScanner
-        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-        style={{height: '50%', width: '70%', left: '15%'}}
-      />
-      
-      <Modal
-        animationType='slide'
-        transparent={true}
-        visible={success}
-      >
-      <View style={styles.centeredViewNew}>
-        <View style={styles.modalViewNew}>
-          <Text style={{fontSize: 14, fontWeight: 'bold', textAlign: 'center', color: 'green'}}>
-          ✅️  Scan Success, Valid Member! 
-          </Text>
-          {/* onPress={() => setShowModal(false)} */}
-        </View>
+      <ActivityIndicator size="large" color="#8B6C5B" style={{marginBottom: '8%'}}/> */}
+      <View style={{marginTop: '15%'}}>
+        <Text style={{color: '#311E15', fontSize: 25, fontWeight: '600', alignSelf: 'center'}}> Saurabh Powar </Text>
+        <Text style={{color: '#8B6C5B', fontSize: 18, fontWeight: '400', alignSelf: 'center', margin: 7}}> Roll No. 191060053 </Text>
       </View>
-      </Modal>
-      <Modal
-        animationType='slide'
-        transparent={true}
-        visible={error}
-        
-      >
-      <View style={styles.centeredViewNew}>
-        <View style={styles.modalViewNew}>
-          <Text style={{fontSize: 14, fontWeight: 'bold', textAlign: 'center', color: 'red'}}>
-          ❌️  Scan Failed, Not Valid Member! 
-          </Text>
-          {/* onPress={() => setShowModal(false)} */}
-        </View>
+      <View style={{alignSelf: 'center', marginTop: '5%'}}>
+        <QRCode
+          size={200}
+          // value={QRTextValue ? QRTextValue : 'NA'}
+          logo={require('../assets/qr_logo.png')}
+          logoSize={50}
+          logoBackgroundColor='transparent'
+          value= "Lodu Utsav 191060052 12012023"
+        />
       </View>
-      </Modal>
-
-      {success && navigation.navigate("AddDetails", {
-            qrData: qrData
-          })}
-      {error && navigation.goBack()}
-      
+      </MainContainer>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
+    // flex: 1,
+    // flexDirection: 'column',
+    // justifyContent: 'center',
   },
-  centeredViewNew: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginBottom: '10%'
-  },
-  modalViewNew: {
-    backgroundColor: '#FFFFFF',
-    borderRadius: 5,
-    padding: 10,
-    width: '80%'
+  upper: {
+    flexDirection: "column",
+    width: "120%",
+    marginLeft: "-10%",
+    marginTop: "-30%",
+    margin: "auto",
+    borderRadius: 200,
+    zIndex: 1,
+    backgroundColor: "#311E15",
   },
 });
